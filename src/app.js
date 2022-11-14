9816e8b5eea157c8e03e44b8c301ddbb8e8a6cc6
Abhishek Gharami <abhishekgharami1998@gmail.com>
2022-11-14 12:37:47 +0530

    setting up for heroku

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const hbs = require('hbs');
 
 const app = express()
 
-const port = 3000 
+const port = process.env.PORT || 3000
 
 const publicDirectoryPath = path.join(__dirname,'../public');
 const partialsPath = path.join(__dirname,'../templates/partials');
